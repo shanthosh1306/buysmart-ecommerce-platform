a77a213c415feb87af056ee5586e469b0b3465cd
Nishanth-12345 <126553537+Nishanth-12345@users.noreply.github.com>
2026-01-06 18:45:54 +0530

    Fix databas.py for second time

--- app/database.py
+++ app/database.py
@@ -3,11 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import text
 
-with engine.connect() as conn:
-    db_name = conn.execute(text("SELECT current_database()")).scalar()
-    schema = conn.execute(text("SELECT current_schema()")).scalar()
-    print("CONNECTED DATABASE =", db_name)
-    print("CURRENT SCHEMA =", schema)
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
@@ -19,5 +15,10 @@ engine = create_engine(
     connect_args={"options": "-csearch_path=public"},
 )
 
+with engine.connect() as conn:
+    db_name = conn.execute(text("SELECT current_database()")).scalar()
+    schema = conn.execute(text("SELECT current_schema()")).scalar()
+    print("CONNECTED DATABASE =", db_name)
+    print("CURRENT SCHEMA =", schema)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
